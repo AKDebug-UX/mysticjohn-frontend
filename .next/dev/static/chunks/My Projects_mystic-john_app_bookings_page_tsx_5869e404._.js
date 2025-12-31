81a47d17d6a3e239441fd4a1d1fa74e566a907a1
Ayotomiwadavid <ayotomiwadavid@proton.me>
2025-12-31 09:59:19 +0000

    saving progress

---
+++ .next/dev/static/chunks/My Projects_mystic-john_app_bookings_page_tsx_5869e404._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/My Projects_mystic-john_4cd0cb9c._.js",
+  "static/chunks/6ccfa_4baed38e._.js"
+],
+    source: "dynamic"
+});
