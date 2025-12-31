c2e94caec6eb2ac483abf812ae9009386af81c63
Ayotomiwadavid <ayotomiwadavid@proton.me>
2025-12-31 09:50:59 +0000

    saving progress

---
+++ .next/dev/static/chunks/My Projects_mystic-john_app_courses_page_tsx_5869e404._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/My Projects_mystic-john_b5d913b9._.js",
+  "static/chunks/6ccfa_48ca7cf7._.js"
+],
+    source: "dynamic"
+});
---
+++ .next/dev/static/chunks/My Projects_mystic-john_app_login_page_tsx_5869e404._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/My Projects_mystic-john_e47f3792._.js",
+  "static/chunks/6ccfa_aa3a58d0._.js"
+],
+    source: "dynamic"
+});
